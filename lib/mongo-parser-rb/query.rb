31ab2d159bed43040affc8a5b5a3dc396b5c3742
Ben McRedmond <ben@benmcredmond.com>
2013-03-27 13:59:19 +0000

    document public api

--- lib/mongo-parser-rb/query.rb
+++ lib/mongo-parser-rb/query.rb
@@ -1,19 +1,34 @@
 module MongoParserRB
   class Query
 
+    # Parse a query, returning an initialized MongoParserRB::Query object.
+    #
+    # @param raw_query [Hash] the hash to parse
+    # @return [MongoParserRB::Query] initialized query object
     def self.parse(raw_query)
       new(raw_query).parse!
     end
 
+    # Use {MongoParserRB::Query.parse}.
+    # @visibility private
     def initialize(raw_query)
       @raw_query = raw_query
     end
 
+    # @visibility private
     def parse!
       @expression_tree = parse_root_expression(@raw_query)
       self
     end
 
+    # Check if a document matches a query.
+    #
+    #     q = MongoParserRB::Query.parse(:x => {:$gt => 3})
+    #     q.matches_document?(:x => 4) => true
+    #     q.matches_document?(:x => 3) => false
+    #
+    # @param document [Hash] The document to check
+    # @return [Boolean]
     def matches_document?(document)
       raise NotParsedError, "Query not parsed (run parse!)" if @expression_tree.nil?
       @expression_tree.evaluate(document)
